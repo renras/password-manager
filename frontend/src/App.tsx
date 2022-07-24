dca8ea54d61dd3a33f0e6f4bc19111b7bd0f1fd8
Renzo Visperas <renzovisperas07@gmail.com>
2022-07-24 22:59:42 +0800

    initialize create a secret form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,23 @@ import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div>
-      <label htmlFor="name">Name</label>
-      <input id="name" type="text" />
-      <label htmlFor="secret">Secret</label>
-      <input id="secret" type="text" />
-      <button>Add</button>
+    <div className="mt-5">
+      <form className="mx-auto d-flex flex-column shadow-sm mw-sm p-5">
+        <h1>Create a Secret</h1>
+        <label htmlFor="key" className="form-label mt-3">
+          Key
+        </label>
+        <input id="key" type="text" className="form-control form-control-lg" />
+        <label htmlFor="value" className="form-label mt-3">
+          Value
+        </label>
+        <input
+          id="value"
+          type="text"
+          className="form-control form-control-lg"
+        />
+        <button className="btn btn-primary mt-5 btn-lg">Submit</button>
+      </form>
     </div>
   );
 }
