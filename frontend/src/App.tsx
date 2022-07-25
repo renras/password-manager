2e0a6ce50ac7f0496e9bc9032bd3396ab9c1de24
Renzo Visperas <renzovisperas07@gmail.com>
2022-07-25 20:25:10 +0800

    add validation to key field

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import axios from "axios";
 import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   useEffect(() => {
     (async () => {
       try {
@@ -16,12 +25,27 @@ function App() {
 
   return (
     <div className="mt-5">
-      <form className="mx-auto d-flex flex-column shadow-sm mw-sm p-5">
+      <form
+        className="mx-auto d-flex flex-column shadow-sm mw-sm p-5"
+        onSubmit={onSubmit}
+      >
         <h1>Create a Secret</h1>
         <label htmlFor="key" className="form-label mt-3">
           Key
         </label>
-        <input id="key" type="text" className="form-control form-control-lg" />
+        <input
+          {...register("key", {
+            required: "Key is required",
+            maxLength: {
+              value: 100,
+              message: "Only 100 characters is allowed",
+            },
+          })}
+          className="form-control form-control-lg"
+        />
+        {typeof errors.key?.message === "string" && (
+          <p className="text-danger m-0">{errors.key?.message}</p>
+        )}
         <label htmlFor="value" className="form-label mt-3">
           Value
         </label>
