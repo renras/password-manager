6fc534f8dc276b6da8fec84c331fdee45b17213f
Renzo Visperas <renzovisperas07@gmail.com>
2022-07-30 22:40:27 +0800

    add react hook form to register form

--- frontend/src/pages/Register/index.tsx
+++ frontend/src/pages/Register/index.tsx
@@ -1,18 +1,68 @@
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface FormData {
+  username: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  const onSubmit = handleSubmit((data) => console.log(data));
+
   return (
     <div>
-      <form className="shadow-sm w-100 mw-sm m-auto mt-5 p-5 d-flex flex-column">
+      <form
+        className="shadow-sm w-100 mw-sm m-auto mt-5 p-5 d-flex flex-column"
+        onSubmit={onSubmit}
+      >
         <h1>Sign Up</h1>
+
+        {/* username */}
         <label htmlFor="username" className="mt-4">
           Username*
         </label>
-        <input id="username" className="form-control form-control-lg mt-1" />
+        <input
+          id="username"
+          className="form-control form-control-lg mt-1"
+          {...register("username", {
+            required: "Username is required",
+            maxLength: {
+              value: 100,
+              message: "Only 100 characters is allowed",
+            },
+          })}
+        />
+        {errors.username?.message && (
+          <p className="text-danger m-0">{errors.username?.message}</p>
+        )}
+
+        {/* email */}
         <label htmlFor="email" className="mt-4">
           Email*
         </label>
-        <input id="email" className="form-control form-control-lg mt-1" />
+        <input
+          id="email"
+          className="form-control form-control-lg mt-1"
+          {...register("email", {
+            required: "Email is required",
+            maxLength: {
+              value: 100,
+              message: "Only 100 characters is allowed",
+            },
+          })}
+        />
+        {errors.email?.message && (
+          <p className="text-danger m-0">{errors.email?.message}</p>
+        )}
+
+        {/* password */}
         <label htmlFor="password" className="mt-4">
           Password*
         </label>
@@ -20,7 +70,19 @@ const Login = () => {
           id="password"
           type="password"
           className="form-control form-control-lg mt-1"
+          {...register("password", {
+            required: "Password is required",
+            maxLength: {
+              value: 100,
+              message: "Only 100 characters is allowed",
+            },
+          })}
         />
+        {errors.password?.message && (
+          <p className="text-danger m-0">{errors.password?.message}</p>
+        )}
+
+        {/* confirm password */}
         <label htmlFor="confirm-password" className="mt-4">
           Confirm Password*
         </label>
@@ -28,7 +90,18 @@ const Login = () => {
           id="confirm-password"
           type="password"
           className="form-control form-control-lg mt-1"
+          {...register("confirmPassword", {
+            required: "Confirm password is required",
+            maxLength: {
+              value: 100,
+              message: "Only 100 characters is allowed",
+            },
+          })}
         />
+        {errors.confirmPassword?.message && (
+          <p className="text-danger m-0">{errors.confirmPassword?.message}</p>
+        )}
+
         <button className="btn btn-primary mt-5 w-100">SIGN UP</button>
         <div className="mt-4 m-auto">
           Already have an account? <Link to="/login">Log in</Link>
