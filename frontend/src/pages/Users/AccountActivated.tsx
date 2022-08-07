f44cefb6d4f36ab329d0b9d5bd1e83524e623372
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-07 22:11:58 +0800

    add logic for acivating user account

--- frontend/src/pages/Users/AccountActivated.tsx
+++ frontend/src/pages/Users/AccountActivated.tsx
@@ -1,6 +1,46 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const AccountActivated = () => {
+  const { uid, token } = useParams();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
+  if (isLoading) {
+    (async () => {
+      try {
+        await axios.post(
+          `${process.env.REACT_APP_DOMAIN}/auth/users/activation/`,
+          {
+            uid: uid,
+            token: token,
+          }
+        );
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    })();
+
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    return (
+      <div>
+        <div className="mw-sm m-auto mt-5 p-5 shadow-sm text-center">
+          <h1>Something went wrong</h1>
+          <p className="fs-5 mt-4">
+            Either the token has expired or the account is already activated.
+          </p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="mw-sm m-auto mt-5 p-5 shadow-sm text-center">
       <h1>Your account has been successfully activated!</h1>
