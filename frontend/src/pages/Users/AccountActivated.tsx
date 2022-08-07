e7089de73f344cb6637b369901dcd8611ed5da7d
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-07 20:47:38 +0800

    add styling to account activated page

--- frontend/src/pages/Users/AccountActivated.tsx
+++ frontend/src/pages/Users/AccountActivated.tsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const AccountActivated = () => {
-  return <div>Account Activated!</div>;
+  return (
+    <div className="mw-sm m-auto mt-5 p-5 shadow-sm text-center">
+      <h1>Your account has been successfully activated!</h1>
+      <p className="fs-5 mt-4">
+        You may now{" "}
+        <Link to="/login" className="link" replace={true}>
+          login
+        </Link>{" "}
+        to access your account
+      </p>
+    </div>
+  );
 };
 
 export default AccountActivated;
