9ea36bc4193c77012885eff4ae36124d4d93ed9b
Renzo Visperas <renzovisperas07@gmail.com>
2022-08-07 19:58:14 +0800

    style activate account message after sign up

--- frontend/src/pages/Users/Activation.tsx
+++ frontend/src/pages/Users/Activation.tsx
@@ -1,5 +1,16 @@
 const Activation = () => {
-  return <div>Activate Email Please!</div>;
+  return (
+    <div className="mw-sm m-auto mt-5 p-5 shadow-sm text-center">
+      <h1>Activate your account</h1>
+      <p className="fs-5 mt-4">
+        We&apos;ve sent you a link in your email to confirm your account
+      </p>
+      <p className="text-secondary mt-5">
+        <b>Haven&apos;t received an email?</b>&nbsp;&nbsp;
+        <button className="d-inline-flex link">Resend activation link</button>
+      </p>
+    </div>
+  );
 };
 
 export default Activation;
